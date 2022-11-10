64156ea390dd3e075fd00e34287bc797532f3477
tarcisiomail <tarcisio.dandias@gmail.com>
2022-11-09 22:15:43 -0300

    First commit

--- main.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from decimal import Decimal
-import num2words
-from extenso import numero_por_extenso
-
-valor = st.number_input(label = "shit")
-nome = st.text_input(label = 'merde')
-
-st.write(numero_por_extenso(valor))

ce25d174ca404a9e16aabcb6171746182b2934ce
tarcisiomail <tarcisio.dandias@gmail.com>
2022-11-14 15:10:36 -0300

    Import libraries Babel and money.

--- app.py
+++ app.py
@@ -9,84 +9,80 @@ from month_translate import translate_month
 import os
 from docx import Document
 import pandas as pd
-keys=0
+
+keys = 0
 random.seed()
 import time
 import streamlit.components.v1 as components
 
 if "new_row" not in st.session_state:
-    st.session_state.new_row = pd.DataFrame(columns=['Item ID', 'Item Descricao', 'Item Quantidade',
-                                                     'Item Unidade','Valor do Item', 'Valor por extenso'])
+    st.session_state.new_row = pd.DataFrame(columns=['Lote ID', 'Item ID', 'Item Descricao', 'Item Quantidade',
+                                                     'Item Unidade', 'Valor do Item', 'Valor por extenso'])
 
 doc = DocxTemplate("MATRIZ.docx")
 
 col1, col2 = st.columns(2)
 with col1:
-    processo = st.text_input(label = 'Número do Processo:', key=1)
-    interessado = st.text_input(label = 'Setor interessado:', key=2)
-    fls_solicitacao = st.text_input(label = 'Folhas da solicitação no processo:', key=3)
-    descricao = st.text_input(label = 'Descrição da Solicitação:', key=4)
-    mod_num_licitacao = st.text_input(label = 'Modo e número da Licitação:', key=5)
-    fls_ata = st.text_input(label = 'Folhas da ata no processo:', key=6)
-    fls_pregoeiro = st.text_input(label = 'Folhas da indicação do pregoeiro no processo:', key=7)
-    id_itens_lote = st.text_input(label = 'Identificação dos lotes:', key=8)
+    date_input = st.date_input("Data do documento:")
+    data_documento = translate_month(date_input.strftime("%d de %B de %Y"))
+    processo = st.text_input(label='Número do Processo:', key=1)
+    interessado = st.text_input(label='Setor interessado:', key=2)
+    fls_solicitacao = st.text_input(label='Folhas da solicitação no processo:', key=3)
+    descricao = st.text_input(label='Descrição da Solicitação:', key=4)
+    mod_num_licitacao = st.text_input(label='Modo e número da Licitação:', key=5)
+    fls_ata = st.text_input(label='Folhas da ata no processo:', key=6)
+    fls_pregoeiro = st.text_input(label='Folhas da indicação do pregoeiro no processo:', key=7)
+
 
 with col2:
-    data_documento = translate_month(datetime.today().strftime("%d de %B de %Y"))
-    ganhadora = st.text_input(label = 'Nome da empresa ganhadora:', key=10)
-    valor_lance = st.number_input(label = 'Valor do lance ganhador:', key=11)
+    id_itens_lote = st.text_input(label='Identificação dos lotes:', key=8)
+    ganhadora = st.text_input(label='Nome da empresa ganhadora:', key=10)
+    valor_lance = st.number_input(label='Valor do lance ganhador:', key=11)
     valor_lance1 = Money(amount=valor_lance, currency='BRL').format('pt_BR')
-    fls_especificacoes = st.text_input(label = 'Folhas contendo as especificações no processo:', key=12)
-    id_compra = st.text_input(label = 'Identificação da compra:', key=13)
-    pregoeiro = st.text_input(label = 'Nome do Pregoeiro:', key=14)
-    cnpj_ganhadora = st.text_input(label = 'CNPJ da empresa ganhadora:', key=15)
+    fls_especificacoes = st.text_input(label='Folhas contendo as especificações no processo:', key=12)
+    id_compra = st.text_input(label='Identificação da compra:', key=13)
+    pregoeiro = st.text_input(label='Nome do Pregoeiro:', key=14)
+    cnpj_ganhadora = st.text_input(label='CNPJ da empresa ganhadora:', key=15)
     data_adjudicacao = st.date_input("Data da Adjudicação:")
 
-
-lote1 = ('1 item\n2 itens\n3 itens.')
-mydict ={}
-print(mydict)
-inicial = [['teste1', 'teste2', 2, 'Dois reais.']]
-df = pd.DataFrame(inicial, columns=['Item ID', 'Item Descricao', 'Valor do Item', 'Valor por extenso'])
-df.loc[len(df)] = ['teste2', 'teste3', 4, 'Quatro reais.']
 lote_button = st.button("manda")
-st.session_state['dataframe']=keys
-st.dataframe(df)
 
 if lote_button:
-    data = translate_month(data_adjudicacao.strftime("%d de %B de %Y"))+"."
+    data = translate_month(data_adjudicacao.strftime("%d de %B de %Y")) + "."
     st.write(data)
-def funcao(a,b,c,d):
+
+
+def funcao(a, b, c, d):
     df.loc[len(df)] = [a, b, c, d]
 
-def item_description (item_id, item_descricao, item_quantidade, item_unidade, item_value, item_full_value):
-    valor_total = item_quantidade * item_value
-    valor_total_full = numero_por_extenso(valor_total)
-    return str(f"{item_id} - {item_descricao}\nQUANTIDADE: {('%.0f' % item_quantidade)} ({item_unidade})\n"
-                f"VALOR UNITÁRIO: R${('%.2f' % item_value).replace('.',',')}\nVALOR TOTAL DO {item_id} ............... R$"
-                f"{('%.2f' % valor_total).replace('.',',')} ({(valor_total_full).replace('.','')}).\n")
+def item_description(item_id, item_descricao, item_quantidade, item_unidade, item_value):
+    valor_total = Money(amount=item_quantidade * item_value, currency='BRL').format('pt_BR')
+    valor_total_full = numero_por_extenso(item_quantidade * item_value)
+    return str(f"\n{item_id} - {item_descricao}\nQUANTIDADE: {('%.0f' % item_quantidade)} ({item_unidade})\n"
+               f"VALOR UNITÁRIO: {Money(amount=item_value, currency='BRL').format('pt_BR')}"
+               f"\nVALOR TOTAL DO {item_id} ............... R$"
+               f"{valor_total} ({(valor_total_full).replace('.', '')}).\n")
 
-numero_lotes = st.slider("Número de Lotes:",min_value=1,max_value=10,value=1)
+numero_lotes = st.slider("Número de Lotes:", min_value=1, max_value=10, value=1)
 if numero_lotes == 1:
     with st.form(key="a"):
-        item_id = st.text_input("Identificação do item:")
+        lote_id = st.text_input("Identificação do lote")
+        item_id = st.text_input("Identificação do Item:")
         item_descricao = st.text_input("Descrição do Item")
         item_quantidade = st.number_input("Quantidade:")
         item_unidade = st.text_input("Unidade:")
         item_value = st.number_input("Valor do Item:")
         item_full_value = numero_por_extenso(item_value)
         btn_form = st.form_submit_button('Manda de novo')
-        df_new = pd.DataFrame({'Item ID': item_id,
+        df_new = pd.DataFrame({'Lote ID': lote_id,
+                               'Item ID': item_id,
                                'Item Descricao': item_descricao,
                                'Item Quantidade': item_quantidade,
                                'Item Unidade': item_unidade,
                                'Valor do Item': item_value,
                                'Valor por extenso': item_full_value}, index=[len(st.session_state.new_row)])
 
-
         if btn_form:
-
-
             st.session_state.new_row = pd.concat([st.session_state.new_row, df_new], axis=0)
             df_new.drop(df_new.tail(1).index, inplace=True)
             df_new = pd.concat([st.session_state.new_row, df_new], axis=0)
@@ -97,7 +93,7 @@ if numero_lotes > 1:
     lote_id = st.text_input("Identificação do Lote:")
     with st.form(key='1'):
 
-        st.write("LOTE"+lote_id)
+        st.write(lote_id)
         item_id = st.text_input("Identificação do item:")
         item_descricao = st.text_input("Descrição do Item")
         item_quantidade = st.number_input("Quantidade:")
@@ -105,17 +101,15 @@ if numero_lotes > 1:
         item_value = st.number_input("Valor do Item:")
         item_full_value = numero_por_extenso(item_value)
         btn_form = st.form_submit_button('Manda de novo')
-        df_new = pd.DataFrame({'Item ID': item_id,
+        df_new = pd.DataFrame({'Lote ID': lote_id,
+                               'Item ID': item_id,
                                'Item Descricao': item_descricao,
                                'Item Quantidade': item_quantidade,
                                'Item Unidade': item_unidade,
                                'Valor do Item': item_value,
                                'Valor por extenso': item_full_value}, index=[len(st.session_state.new_row)])
 
-
         if btn_form:
-
-
             st.session_state.new_row = pd.concat([st.session_state.new_row, df_new], axis=0)
             df_new.drop(df_new.tail(1).index, inplace=True)
             df_new = pd.concat([st.session_state.new_row, df_new], axis=0)
@@ -126,14 +120,43 @@ finaliza = st.button("Acabou?")
 if finaliza:
     df_new = pd.concat([st.session_state.new_row, df_new], axis=0)
     df_new.drop(df_new.tail(1).index, inplace=True)
+    df_new.sort_values(by=['Lote ID', 'Item ID']).reset_index(drop=True)
     st.dataframe(df_new)
-    #st.write(df_new.loc[len(df_new)-1,'Valor do Item'])
-    #st.dataframe(df_new.loc['2'].at['Valor do Item'])
-    list = {}
-    for i in range(len(df_new)):
-        list['item{0}'.format(i)] = (item_description(df_new.loc[i, 'Item ID'], df_new.loc[i, 'Item Descricao'],
-                                         df_new.loc[i, 'Item Quantidade'], df_new.loc[i, 'Item Unidade'],
-                                         df_new.loc[i, 'Valor do Item'], df_new.loc[i, 'Valor por extenso']))
+    # st.write(df_new.loc[len(df_new)-1,'Valor do Item'])
+    # st.dataframe(df_new.loc['2'].at['Valor do Item'])
+    list_dfs = []
+    lotes = df_new['Lote ID'].unique()
+    for y in range(len(lotes)):
+        list_dfs.append(y)
+        list_dfs[y] = df_new[df_new['Lote ID'] == lotes[y]].reset_index(drop=True)
+
+    list_item = {}
+    list_lote = {}
+    list_total_lote = {}
+    #for z in range(len(lotes)):
+    #    st.dataframe(list_dfs[z])
+    st.write(list_dfs[0].loc[0, 'Item ID'])
+    st.write(list_dfs[1].loc[0, 'Item ID'])
+    st.write(list_dfs[1].loc[1, 'Item ID'])
+    st.write(list_dfs[2].loc[0, 'Item ID'])
+    st.write(list_dfs[2].loc[1, 'Item ID'])
+    st.write(list_dfs[2].loc[2, 'Item ID'])
+
+    for x in range(len(lotes)):
+        lote_atual = 'lote' + str(x)
+        list_lote['lote{0}'.format(x)] = (str(lotes[x]))
+
+        total_lote = (list_dfs[x]['Item Quantidade'] * list_dfs[x]['Valor do Item']).sum()
+        list_total_lote['total_lote{0}'.format(x)] = str(f'\nTOTAL DO {lotes[x]} .... R${total_lote} '
+                                                         f'({numero_por_extenso(total_lote)}).')
+
+        for i in range(len(list_dfs[x])):
+            list_item[(lote_atual + ('item{0}'.format(i)))] = \
+                (item_description(list_dfs[x].loc[i, 'Item ID'],
+                                  list_dfs[x].loc[i, 'Item Descricao'],
+                                  list_dfs[x].loc[i, 'Item Quantidade'],
+                                  list_dfs[x].loc[i, 'Item Unidade'],
+                                  list_dfs[x].loc[i, 'Valor do Item']))
 
     context = {'processo': processo, 'interessado': interessado, 'fls_solicitacao': fls_solicitacao,
                'descricao': descricao, 'data_documento': data_documento,
@@ -141,22 +164,26 @@ if finaliza:
                'id_itens_lote': id_itens_lote, 'ganhadora': ganhadora,
                'valor_lance': valor_lance1,
                'fls_especificacoes': fls_especificacoes, 'id_compra': id_compra, 'pregoeiro': pregoeiro,
-               'cnpj_ganhadora': cnpj_ganhadora, 'data_adjudicacao': data_adjudicacao}
+               'cnpj_ganhadora': cnpj_ganhadora, 'data_adjudicacao':
+                   translate_month(data_adjudicacao.strftime("%d de %B de %Y"))}
 
-    context.update(list)
+    context.update(list_item)
+    context.update(list_lote)
+    context.update(list_total_lote)
 
     df = pd.DataFrame.from_dict([context])
     st.dataframe(df)
+    st.write(list_lote)
+    st.write(list_item)
+    st.write(list_total_lote)
     print(type(context))
     doc.render(context)
     doc.save("generated_doc_2.docx")
 
-
-
 with open("generated_doc_2.docx", "rb") as myfile:
     btn = st.download_button(
         label="Download do documento",
-        data = myfile,
-        file_name = 'new_file.docx',
-        mime = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        data=myfile,
+        file_name='new_file.docx',
+        mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
     )
--- extenso.py
+++ extenso.py
@@ -1,73 +1,72 @@
 from num2words import num2words
 from decimal import Decimal
-import streamlit as st
 
-def numero_por_extenso(numero):
+
+def numero_por_extenso(number):
+    numero = round(number / 1, 2)
     if numero < 1:
         if 0 < numero%1 < 0.0199:
-            valor_extenso = ('Um centavo.')
+            valor_extenso = ('um centavo')
             return valor_extenso
         else:
             valor_decimal = round(numero * 100,2)
             num_ptbr = num2words(Decimal(valor_decimal), lang='pt_BR')
-            valor_extenso = (num_ptbr + ' centavos.').capitalize()
+            valor_extenso = (num_ptbr + ' centavos')
             return valor_extenso
 
     elif numero == 1:
-        valor_extenso = 'Um real.'
+        valor_extenso = 'um real'
         return valor_extenso
 
     elif (numero > 1.00) and (numero < 1.011):
-        valor_extenso = 'Um real e um centavo.'
+        valor_extenso = 'um real e um centavo'
         return valor_extenso
 
     elif (numero > 1.011) and (numero < 2):
         valor_x_100 = round(numero * 100,2)
         valor_decimal = round(valor_x_100 % 100,2)
         num_ptbr = num2words(Decimal(valor_decimal), lang='pt_BR')
-        valor_extenso = ('Um real e ' + num_ptbr + ' centavos.')
+        valor_extenso = ('um real e ' + num_ptbr + ' centavos')
         return valor_extenso
 
     elif (numero < 1000000) or (numero >= 1000002):
         if numero%1 == 0:
             num_ptbr = num2words(Decimal(numero), lang='pt_BR')
-            valor_extenso = (num_ptbr + ' reais.').capitalize()
-            return (valor_extenso).replace("Mil,","Mil")
+            valor_extenso = (num_ptbr + ' reais')
+            return (valor_extenso).replace("Mil,","mil")
 
         elif (numero%1 > 0) and (numero%1 < 0.02):
             num_ptbr = num2words(Decimal(numero // 1), lang='pt_BR')
-            valor_extenso = (num_ptbr + ' reais e um centavo.').capitalize()
-            return (valor_extenso).replace("Mil,","Mil")
+            valor_extenso = (num_ptbr + ' reais e um centavo')
+            return (valor_extenso).replace("Mil,","mil")
 
         else:
 
             num_int_extenso = num2words(Decimal(numero // 1), lang='pt_BR')
             num = round((numero % 1*100),2)
             num_dec_extenso = num2words(Decimal(num), lang='pt_BR')
-            valor_extenso = ((num_int_extenso + ' reais e ' + num_dec_extenso + ' centavos.')\
-                .replace('zero vírgula','').replace('  ', ' ').capitalize())
-            return valor_extenso.replace("Mil,","Mil")
+            valor_extenso = ((num_int_extenso + ' reais e ' + num_dec_extenso + ' centavos')\
+                .replace('zero vírgula','').replace('  ', ' '))
+            return valor_extenso.replace("Mil,","mil")
 
     elif (1000000 <= numero < 1000001):
         if numero == 1000000:
-            return 'Um milhão de reais.'
+            return 'um milhão de reais'
         elif 1000000 < numero < 1000000.02:
-            return 'Um milhão de reais e um centavo.'
+            return 'um milhão de reais e um centavo'
         else:
             num = round((numero % 1 * 100), 2)
             num_dec_extenso = num2words(Decimal(num), lang='pt_BR')
-            valor_extenso = 'Um milhão de reais e ' + num_dec_extenso + ' centavos.'
+            valor_extenso = 'um milhão de reais e ' + num_dec_extenso + ' centavos'
             return valor_extenso
 
     elif (1000001 <= numero < 1000002):
         if numero == 1000001.00:
-            return 'Um milhão e um reais.'
+            return 'um milhão e um reais'
         elif (1000001.00 < numero < 1000001.02):
-            return 'Um milhão e um reais e um centavo.'
+            return 'um milhão e um reais e um centavo'
         else:
             num = round((numero % 1 * 100), 2)
             num_dec_extenso = num2words(Decimal(num), lang='pt_BR')
-            valor_extenso = 'Um milhão e um reais e ' + num_dec_extenso + ' centavos.'
+            valor_extenso = 'um milhão e um reais e ' + num_dec_extenso + ' centavos'
             return valor_extenso
-
-print(numero_por_extenso(10.15))
